78df574dab29503f257abadb1047836f304b79a0
programuesihkb <elion.programhkb@gmail.com>
2024-05-02 23:30:20 +0200

    bug_fix_dublicateNote

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const btnEl = document.getElementById("btn");
 
 let user = JSON.parse(sessionStorage.getItem('user'));
 if (user) {
+  // If user is logged in, display a welcome message
+  document.getElementById('welcome').textContent = `Welcome, ${user.username}!`;
+
   let userNotes = getUserNotes(user.id);
   userNotes.forEach(note => {
     const noteEl = createNoteEl(note.id, note.noteTitle, note.noteText);
@@ -13,12 +16,14 @@ if (user) {
   });
 
   btnEl.addEventListener("click", () => addNote(appEl, btnEl));
-}
+} else {
+    // If user is not logged in, redirect to login.html
+    window.location.href = 'login.html';
+  }
+
 
 const logoutButton = document.getElementById('logout_btn');
 logoutButton.addEventListener('click', function(event) {
     sessionStorage.removeItem('user');
     window.location.href = 'login.html';
 });
-
-export {createNoteEl};
