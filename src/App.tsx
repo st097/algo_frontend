132e43f98ff86e8984c9490d49644296595f18b3
programuesihkb <elion.programhkb@gmail.com>
2024-05-20 12:37:37 +0200

    20 05 24

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useState } from 'react';
+import words from './assets/wordList.json';
+import { HangmanDrawing } from './HangmanDrawing';
+import { HangmanWord } from './HangmanWord';
+import { Keyboard } from './Keyboard';
+
+
 function App() {
-  return <hi>Hello Wolrd!</hi>
+
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  
+  const [guessedLetters, setGessedLetters] = useState<string[]>([]);  
+
+  return <div style={{
+    maxWidth: '800px',
+    margin: '0 auto',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '2rem',
+    alignItems: 'center',
+    }}>
+      <div style={{
+        fontSize: '2rem',
+        textAlign:'center'
+      }}>
+        Lose Win
+      </div>
+
+      {/* hangman components */}
+      <HangmanDrawing/>
+      <HangmanWord/>
+      <Keyboard/>
+
+
+
+  </div>
 }
 
 export default App
---
+++ src/HangmanWord.tsx
@@ -0,0 +1,12 @@
+export function HangmanWord(){
+    return <div style={{
+        display:"flex",
+        gap:".25em",
+        fontSize:"6rem",
+        fontFamily:"monospace",
+        fontWeight:"bold",
+        textTransform:"uppercase"
+    }}>
+        test
+    </div>
+}
